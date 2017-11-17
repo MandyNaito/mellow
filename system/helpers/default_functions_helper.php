e165db78a166d12b46a0af1037bff13194e2200d
Amanda Naito <amanda@agencia.red>
2017-11-17 10:49:46 -0200

    explorar

--- system/helpers/default_functions_helper.php
+++ system/helpers/default_functions_helper.php
@@ -8,12 +8,11 @@ if ( ! function_exists('rand_card_colors'))
 {
 	function rand_card_colors($cont)
 	{
-		if($cont % 4 == 0)
-			return "orange";
-		if($cont % 3 == 0)
-			return "green"; 
-		if($cont % 2 == 0)
-			return "blue"; 
-		return "red";
+		if(intval($cont) % 3 == 0)
+			return "orange"; 
+		if(intval($cont) % 2 == 0)
+			return "cyan"; 
+		
+		return "pink";
 	}
 }
