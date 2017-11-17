ffbdd6c7f2a4900460b59e330eb51e4433d9cd16
Amanda Naito <amanda@agencia.red>
2017-11-17 09:29:23 -0200

    utf-8

--- application/admin/views/templates/header.php
+++ application/admin/views/templates/header.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 		<meta name="description" content="">
 		<meta name="keywords" content="">
 		<meta name="author" content="">
