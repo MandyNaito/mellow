4e30faaa659f9d52486472a467ff2b85c7d339a8
Amanda Naito <amanda@agencia.red>
2017-11-17 10:49:46 -0200

    explorar

--- application/admin/views/estabelecimento/explorar.php
+++ application/admin/views/estabelecimento/explorar.php
@@ -7,11 +7,11 @@
 					<h2><?=$this->breadcrumbs->show();?></h2>
 				</div>
 				<div class="row clearfix">
-					<?php foreach($data_estabelecimento as $estabelecimento){ ?>
+					<?php foreach($data_estabelecimento as $key => $estabelecimento){ ?>
 						<a href="<?=site_url($controller).'/visualizar/'.$estabelecimento['cdestabelecimento'];?>">
 							<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
 								<div class="card-box hover-expand-effect">
-									<div class="icon" style="background-image: url('<?=base_url((!empty($estabelecimento['txfoto']) ? $estabelecimento['txfoto'] : 'assets/images/business-default.png'));?>');"></div>
+									<div class="icon bg-<?=rand_card_colors($key);?>" style="background-image: url('<?=base_url((!empty($estabelecimento['txfoto']) ? $estabelecimento['txfoto'] : 'assets/images/business-default.png'));?>');"></div>
 									<div class="content">
 										<div class="text"><?=$estabelecimento['nmtipoestabelecimento'];?></div>
 										<div class="title"><?=$estabelecimento['nmfantasia'];?></div>
